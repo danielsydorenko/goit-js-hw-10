5f3d0aa9a5b5b04b85aa9b265598b17d4b3c7b4c
danielsydorenko <danielsydorenko1@gmail.com>
2025-09-26 14:48:01 +0200

    homework 10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(form.delay.value);
+  const state = form.state.value;
+
+  createPromise(delay, state)
+    .then(ms => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(ms => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${ms}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
